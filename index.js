b3237ea95c33f45c4a6c7846e24b8319cea77c92
Koustubh-lapate <koustubh.lapate@gmail.com>
2023-10-02 00:15:02 +0530

    Basic js programs added

---
+++ index.js
@@ -0,0 +1,35 @@
+//print statement
+console.log('Hello World Koustubh');
+
+//Variables and constants
+let name = 'Koustubh Lapate';
+var age = 20;
+let year = 4;
+
+const degree = 'B.Tech';
+const field = 'EEE';
+
+//for and while loops - for() and while() - syntax same as c++ and java
+
+//objects
+let person = {
+    firstName : 'Koustubh',
+    lastName : 'Lapate',
+    city : 'Pune',
+    pincode : 411046 
+};
+
+//Arrays
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(arr.length);
+
+//functions
+function greet(){
+    console.log('Hello World');
+}
+
+function square(num){
+    return num * num;
+}
+
+console.log(square(2));
